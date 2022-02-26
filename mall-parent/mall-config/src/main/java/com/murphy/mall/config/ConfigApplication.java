94b8ae8c016554e76be4d0e855a6f7c2e28bffa9
murphy <631341307@qq.com>
2022-02-26 21:54:20 +0800

    【功能开发】配置中心微服务

---
+++ mall-parent/mall-config/src/main/java/com/murphy/mall/config/ConfigApplication.java
@@ -0,0 +1,22 @@
+package com.murphy.mall.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * 配置中心 - 启动类
+ *
+ * @author murphy
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableConfigServer
+public class ConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
+    }
+
+}
