d27851303cfac775a53520a6c66c6ba74d4c054c
murphy <631341307@qq.com>
2022-02-26 21:35:33 +0800

    【功能开发】配置Eureka注册中心

---
+++ mall-parent/mall-registry/src/main/java/com/murphy/mall/registry/RegistryApplication.java
@@ -0,0 +1,20 @@
+package com.murphy.mall.registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 注册中心 - 启动类
+ *
+ * @author murphy
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class RegistryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegistryApplication.class, args);
+    }
+
+}
